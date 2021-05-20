c064661179ddc4f44c2ea34c20f10c7ab035379c
Harith Albadry <harith.albadry@gmail.com>
2021-05-20 16:04:34 +0100

    added more routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,16 +34,36 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {const cat = await Category.create(req.body);
+  res.status(200).json(cat);}
+  catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+try{
+  const delCat = await Category.destroy({
+    where:{
+      id:req.params.id,
+    },
+  });
+  if(!delCat){
+    res.status(404).json({message:"No category with this is"});
+    return;
+  }
+  res.status(200).json(delCat);
+} catch(err){
+  res.status(500).json(err);
+}
+
 });
 
 module.exports = router;
